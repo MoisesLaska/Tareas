ea2d595a5f5ce071d3706d49c10f3325afa06b28
Moises Laska <spwndigital@hotmail.com>
2016-05-20 12:01:36 -0500

    Initial Commit

---
+++ Tarea/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tarea
+//
+//  Created by Moises Laska on 20/05/16.
+//  Copyright © 2016 Moises. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tarea/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tarea
+//
+//  Created by Moises Laska on 20/05/16.
+//  Copyright © 2016 Moises. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
